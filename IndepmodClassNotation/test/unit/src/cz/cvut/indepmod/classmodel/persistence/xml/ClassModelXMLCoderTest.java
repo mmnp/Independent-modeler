22ba50dc58487306e7e94124e8295b5731c1618d
Lucky-V <Lucky.V@gmail.com>
2010-11-04 12:19:39 +0100

    Persistence test

--- IndepmodClassNotation/test/unit/src/cz/cvut/indepmod/classmodel/persistence/xml/ClassModelXMLCoderTest.java
+++ IndepmodClassNotation/test/unit/src/cz/cvut/indepmod/classmodel/persistence/xml/ClassModelXMLCoderTest.java
@@ -24,6 +24,7 @@ import static org.junit.Assert.*;
 public class ClassModelXMLCoderTest {
 
     public static final String CLASS_NAME = "TestClass";
+    public static final String CLASS_NAME2 = "CamelCaseXXX";
     public static final String FILE_NAME = "TestClass";
 
     private ClassModelGraph graph;
@@ -46,27 +47,44 @@ public class ClassModelXMLCoderTest {
     @Test
     public void testSimpleEncodeDecode() throws FileNotFoundException {
         DefaultGraphCell cell = new DefaultGraphCell(new ClassModel(CLASS_NAME));
+        DefaultGraphCell cell2 = new DefaultGraphCell(new ClassModel(CLASS_NAME2));
         GraphConstants.setBounds(cell.getAttributes(), new Rectangle.Double(10, 10, 100, 60));
+        GraphConstants.setBounds(cell2.getAttributes(), new Rectangle.Double(100, 25, 100, 60));
         this.graph.getGraphLayoutCache().insert(cell);
+        this.graph.getGraphLayoutCache().insert(cell2);
+
 
         ClassModelXMLCoder encoder = new ClassModelXMLCoder();
         encoder.encode(this.graph.getGraphLayoutCache(), FILE_NAME);
 
         this.graph.setGraphLayoutCache(encoder.decode(FILE_NAME));
 
-        assertEquals(1, this.graph.getModel().getRootCount());
+        assertEquals(2, this.graph.getModel().getRootCount());
 
-        boolean isThere = false;
+        boolean isThereClass1 = false;
+        boolean isThereClass2 = false;
         for (TypeModel m : this.graph.getAllTypes()) {
             if (m.getTypeName().equals(CLASS_NAME)) {
-                isThere = true;
+                isThereClass1 = true;
+            }
+
+            if (m.getTypeName().equals(CLASS_NAME2)) {
+                isThereClass2 = true;
             }
         }
-        assertTrue(isThere);
+        assertTrue("Cell 1 is not there after decode", isThereClass1);
+        assertTrue("Cell 2 is not there after decode", isThereClass1);
 
         DefaultGraphCell root = (DefaultGraphCell)this.graph.getRoots()[0];
         ClassModel model = (ClassModel)root.getUserObject();
         assertEquals(CLASS_NAME, model.getTypeName());
+        assertTrue(model.getAttributeModels().isEmpty());
+        assertTrue(model.getMethodModels().isEmpty());
+
+        root = (DefaultGraphCell)this.graph.getRoots()[1];
+        model = (ClassModel)root.getUserObject();
+
+        assertEquals(CLASS_NAME2, model.getTypeName());
         assertTrue(model.getAttributeModels().isEmpty());
         assertTrue(model.getMethodModels().isEmpty());
     }
