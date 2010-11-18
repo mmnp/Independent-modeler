6cdb920fe981fdfb43935849d1220c7b91337602
hegladan <hegladan@fel.cvut.cz>
2010-11-18 15:44:16 +0100

    Repair hegladan branch.

--- IndepmodClassNotation/src/cz/cvut/indepmod/classmodel/resources/Resources.java
+++
@@ -1,9 +0,0 @@
-package cz.cvut.indepmod.classmodel.resources;
-
-public class Resources {
-
-    public static final String ICONS = "icons/";
-    public static final String DIAGRAM = "diagram.png";
-    public static final String PREVIEW = "preview.png";
-
-}
